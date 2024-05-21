d879e0c9e62c5d5873c607a4d0ec1a753b87efca
0Shinchan <joydiprakshit2004@gmail.com>
2024-05-21 21:29:45 +0530

    dfs, iterative

---
+++ dfs_iterative.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class graph{
+    public:
+    map<int ,bool>visit;
+    map<int ,list<int> >adj;
+    
+    void addEdge(int u,int v){
+        adj[u].push_back(v);
+    }
+
+    void dfs(int s){
+        vector<int> visited(4,false);
+        stack<int> st;
+        
+        st.push(s);
+
+        while(!st.empty()){
+            int s = st.top();
+
+            st.pop();
+            
+            if(!visited[s]){
+
+                visited[s]=true;
+                cout<< s <<" ";
+            }
+            for(auto i=adj[s].begin(); i!=adj[s].end();i++){
+                if(!visited[*i]){
+                    st.push(*i);
+                }
+            }
+        }
+
+    }
+};
+
+int main(){
+
+    graph g;
+      
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Depth First Traversal"
+            " (starting from vertex 2) \n";
+ 
+    // Function call
+    g.dfs(2);
+
+    return 0;
+ 
+}
